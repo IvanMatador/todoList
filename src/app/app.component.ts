7ce5db388c134ff3822cc7c86d9c7d4569d7b840
Ivan <ivan.zf.netc@gmail.com>
2020-10-20 18:06:40 +0300

    added todo Component with *ngFor

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,22 @@
 import { Component } from '@angular/core';
 
+export interface Todo {
+  id: number
+  title: string
+  complited: boolean
+  date: Date
+}
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'todoList';
+  appTitle = 'Todo Angular application';
+  public todos: Todo[] = [
+    {id: 1, title: 'first string', complited: false, date: new Date()},
+    {id: 2, title: 'second string', complited: true, date: new Date()},
+    {id: 3, title: 'last string', complited: false, date: new Date()}
+  ]
 }
